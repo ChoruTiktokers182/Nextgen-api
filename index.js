b29c73a2253c2772cbec0e82e41bdc5a83ba4c0b
CHORU-OFFICIAL <112081055+ChoruTiktokers182@users.noreply.github.com>
2024-06-23 20:22:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
